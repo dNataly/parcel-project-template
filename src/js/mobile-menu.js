edf488856a2b49d7017d7147643db65dec24899e
Serhii Krimets <krimets.s88@gmail.com>
2021-03-16 21:54:48 +0200

    update modal js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,11 @@
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
     const mobileBtnClose = document.querySelector("[data-menu-close]");
+    const mobileBtnClose2 = document.querySelector("[data-menu-close2]");
+    const mobileBtnClose3 = document.querySelector("[data-menu-close3]");
+    const mobileBtnClose4 = document.querySelector("[data-menu-close4]");
+    const mobileBtnClose5 = document.querySelector("[data-menu-close5]");
+    const mobileBtnClose6 = document.querySelector("[data-menu-close6]");
   
     menuBtnRef.addEventListener("click", () => {
       mobileMenuRef.classList.toggle("is-open");
@@ -9,5 +14,25 @@
 
     mobileBtnClose.addEventListener('click', () => {
       mobileMenuRef.classList.toggle("is-open");
+    });
+  
+    mobileBtnClose2.addEventListener('click', () => {
+        mobileMenuRef.classList.toggle("is-open");
+    });
+    
+    mobileBtnClose3.addEventListener('click', () => {
+        mobileMenuRef.classList.toggle("is-open");
+    });
+    
+    mobileBtnClose4.addEventListener('click', () => {
+        mobileMenuRef.classList.toggle("is-open");
+    });
+    
+    mobileBtnClose5.addEventListener('click', () => {
+        mobileMenuRef.classList.toggle("is-open");
+    });
+    
+    mobileBtnClose6.addEventListener('click', () => {
+        mobileMenuRef.classList.toggle("is-open");
     });
   })()
