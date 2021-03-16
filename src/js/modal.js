36e62e3bc0a83e1562827465de3d90b79846c64d
Serhii Krimets <krimets.s88@gmail.com>
2021-03-16 05:19:04 +0200

    update-modal-btn

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,16 @@
 (() => {
   const refs = {
+    openModalBtnHead: document.querySelector('[data-modal-open-head]'),
+    openModalBtnMobile: document.querySelector('[data-modal-open-mobile]'),
     openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    closeModalBtn: document.querySelector('[data-modal-close1]'),
+    modal: document.querySelector('[data-modal1]'),
   };
 
+  refs.openModalBtnHead.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnMobile.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
