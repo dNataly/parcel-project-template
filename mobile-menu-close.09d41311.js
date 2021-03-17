81cc7c1308ad7d6ecd7a5898c4e972621a8d4be1
natalia_denysenko <nnmtshh@gmail.com>
2021-03-17 09:56:30 +0200

    Updates

--- mobile-menu-close.09d41311.js
+++ mobile-menu-close.09d41311.js
@@ -117,79 +117,15 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
+})({"js/mobile-menu-close.js":[function(require,module,exports) {
+$(document).mouseup(function (e) {
+  var div = $('.mobile-menu');
 
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
+  if (!div.is(e.target) && div.has(e.target).length === 0) {
+    $('.backdrop').removeClass('is-open');
   }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"slick/slick-theme.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"./ajax-loader.gif":[["ajax-loader.7d17f143.gif","slick/ajax-loader.gif"],"slick/ajax-loader.gif"],"./fonts/slick.eot":[["slick.c3ee5f0f.eot","slick/fonts/slick.eot"],"slick/fonts/slick.eot"],"./fonts/slick.woff":[["slick.c84d2f04.woff","slick/fonts/slick.woff"],"slick/fonts/slick.woff"],"./fonts/slick.ttf":[["slick.1456673f.ttf","slick/fonts/slick.ttf"],"slick/fonts/slick.ttf"],"./fonts/slick.svg":[["slick.ad7ee52d.svg","slick/fonts/slick.svg"],"slick/fonts/slick.svg"],"./../images/home.svg":[["home.941c208c.svg","images/home.svg"],"images/home.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+});
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -393,5 +329,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/slick-theme.774411c1.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/mobile-menu-close.js"], null)
+//# sourceMappingURL=/mobile-menu-close.09d41311.js.map
